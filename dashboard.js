147b253ef762d3e574745933b92ced500a8278ea
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-08-19 16:42:15 -0700

    deleted files

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-let pass = document.querySelector("#password_reveal");
-console.log(pass)
-
-pass.addEventListener("click",function(){
-    let shown_password = document.querySelector(".password_hidden");
-    shown_password.textContent ="kyronnyoro1"
-    shown_password.classList.add("password_font");
-})
