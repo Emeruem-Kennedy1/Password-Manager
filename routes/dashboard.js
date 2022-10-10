c9f1caf53388c96f286bf4449df47e2916d99014
Emeruem Kennedy <69385717+Emeruem-Kennedy1@users.noreply.github.com>
2022-10-10 13:39:15 -0700

    dashboard backend

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,17 +2,38 @@ const exppress = require('express');
 const router = exppress.Router();
 const {checkAuthenticated} = require('../auth/checkAuthenticated');
 
-services = [
-    {
-        brandname: 'Google',
-        serviceName: 'Google Drive',
-        username: 'kendo',
-        password: 'kendo',
-    }
-]
-router.get('/', checkAuthenticated, (req, res) => {
-    res.render('dashboard',services);
+
+const findServices = async (emailID) =>{
+    const url = `${process.env.API_ENTRYPOINT}/${process.env.API_KEY}/users/${emailID}/services`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+router.get('/', checkAuthenticated, async (req, res) => {
+    const services = await findServices(req.user.email);
+
+    res.render('dashboard', {services: services.data});
+
 })
+
+router.post('/', checkAuthenticated, async (req, res) => {
+    const url = `${process.env.API_ENTRYPOINT}/${process.env.API_KEY}/users/${req.user.email}/services`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            serviceName: req.body.serviceName,
+            username: req.body.username,
+            password: req.body.password
+        })
+    });
+    const data = await response.json();
+    res.redirect('/dashboard');
+})
+
 
 
 module.exports = router;
